19fc6c878c013279b587ff8c51e6eda1455811bb
Adam Fedor <afedor@t1v.com>
2016-05-25 14:32:16 -0600

    Auto-box XPC errors into a NSDictionary

--- XPCKit/NSObject+XPCParse.m
+++ XPCKit/NSObject+XPCParse.m
@@ -47,7 +47,16 @@
 		object = [NSFileHandle fileHandleWithXPCObject:xpcObject];
     }else if(type == XPC_TYPE_BOOL || type == XPC_TYPE_UINT64 || type == XPC_TYPE_INT64 || type == XPC_TYPE_DOUBLE){
         object = [NSNumber numberWithXPCObject:xpcObject];
-	}
+    }
+    else if([[(__bridge NSObject *)type className] isEqualToString: @"OS_xpc_mach_send"]){
+        object = [XPCIOSurface surfaceRefWithXPCObject: xpcObject];
+    }
+    else if (xpcObject == XPC_ERROR_CONNECTION_INTERRUPTED ||
+             xpcObject == XPC_ERROR_CONNECTION_INVALID ||
+             xpcObject == XPC_ERROR_KEY_DESCRIPTION ||
+             xpcObject == XPC_ERROR_TERMINATION_IMMINENT) {
+        object = [NSDictionary dictionaryWithContentsOfXPCObject: xpcObject];
+    }
     return object;
 }
 
